05f96bd62885ae16c90dce5b809023ea13878784
Stephan Diehl <stephan@transvection.de>
2019-04-04 17:41:48 +0200

    initial Tax model

---
+++ fsharp/ECommerce/src/TaxLib/TaxLib.fs
@@ -0,0 +1,38 @@
+﻿module TaxLib
+
+type ISOCountryCode = ISOCountryCode of string
+
+type ZipCode = ZipCode of string
+
+type StateCounty = StateCounty of string
+
+type TaxRegion =
+  | DE
+  | EU
+  | NON_EU
+
+type ProductType =
+  | Hardware
+  | Software
+  | Book
+
+type CustomerType =
+  | B2C
+  | B2B
+  | IC
+
+type TaxRate = TaxRate of decimal
+
+type TaxZoneId = TaxZoneId string
+
+type TaxZone = {
+  TaxZoneId: TaxZoneId
+  Region: TaxRegion
+  CountryCode: ISOCountryCode
+  State: StateCountry
+  ZipCodes: ZipCode list
+}
+
+type Tax = {
+  TaxZoneId: TaxZoneId
+}
