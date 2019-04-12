380e9c97418f6401aae300cf0ec5c9d630e7bc6c
std <stephan.diehl@ableton.com>
2019-04-12 14:48:12 +0000

    add useful tests

--- fsharp/ECommerce/tests/ECommerceTests/ECommerceTests.fs
+++ fsharp/ECommerce/tests/ECommerceTests/ECommerceTests.fs
@@ -1,11 +1,20 @@
 module Tests
 
 open Expecto
-open TaxLib
+open ECommerce.TaxAddress
 
 [<Tests>]
 let tests =
-  testList "Tax Tests" [
-    testCase "trivial" <| fun _ ->
-      Expect.equal "a" "a" "a"
-  ]
+    testList "Tax Tests" [
+        test "de" {
+            let res = createTaxAddress "de" "" ""
+            let expected = Ok (Country (ISOCountryCode "de"))
+            Expect.equal res expected "create german tax address"
+        }
+
+        test "d" {
+            let res = createTaxAddress "d" "" ""
+            let expected = Error "Country Code needs to be a 2 char ISO country code"
+            Expect.equal res expected "create address with non ISO CC"
+        }
+    ]
